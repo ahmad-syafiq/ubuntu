ead86d0c479ce3b046114184b22b8cf0a78c6d7e
syafiq <syafiq@fisip.net>
2019-01-04 11:25:35 +0700

    edit script install telegram desktop

--- setup ubuntu 18.04.sh
+++ setup ubuntu 18.04.sh
@@ -30,7 +30,32 @@ sudo gdebi google-chrome-stable_current_amd64.deb
 
 clear && clear
 echo '## Install Telegram Desktop'
-sudo apt-get install -y telegram-desktop
+cd ~/tmp/
+wget -O - https://telegram.org/dl/desktop/linux > tsetup.tar.gz
+wget -O - https://raw.githubusercontent.com/telegramdesktop/tdesktop/master/Telegram/Telegram/Images.xcassets/Icon.iconset/icon_256x256@2x.png > icon.png
+sudo mkdir /usr/share/telegram
+sudo chmod +x /usr/share/telegram
+tar -xvJf tsetup.tar.gz
+sudo echo "[Desktop Entry]" > telegram.desktop
+sudo echo "Name=Telegram" >> telegram.desktop
+sudo echo "GenericName=Chat" >> telegram.desktop
+sudo echo "Comment=Chat with yours friends" >> telegram.desktop
+sudo echo "Exec=/usr/share/telegram/Telegram" >> telegram.desktop
+sudo echo "Terminal=false" >> telegram.desktop
+sudo echo "Type=Application" >> telegram.desktop
+sudo echo "Icon=/usr/share/telegram/icon.png" >> telegram.desktop
+sudo echo "Categories=Network;Chat;" >> telegram.desktop
+sudo echo "StartupNotify=false" >> telegram.desktop
+sudo cp icon.png /usr/share/telegram/icon.png
+sudo cp telegram.desktop /usr/share/applications/telegram.desktop
+cd ./Telegram
+sudo cp ./Updater /usr/share/telegram/Updater
+sudo cp ./Telegram /usr/share/telegram/Telegram
+rm ~/tmp/tsetup.tar.gz
+rm ~/tmp/icon.png
+rm ~/tmp/telegram.desktop
+rm -R ~/tmp/Telegram
+# sudo apt-get install -y telegram-desktop
 
 
 clear && clear
